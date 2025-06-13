c3c5ed9d92e31f6d4014b5b2f720bdd0ed76e1d8
oumaima27 <maghraouioumaima0@gmail.com>
2025-06-13 13:41:35 +0100

    Remove failing test file

--- springboot-swagger-crud/src/test/java/com/springswagger/SpringbootSwaggerCrudApplicationTests.java
+++ springboot-swagger-crud/src/test/java/com/springswagger/SpringbootSwaggerCrudApplicationTests.java
@@ -1,6 +1,6 @@
 package com.springswagger;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +9,4 @@ class SpringbootSwaggerCrudApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
