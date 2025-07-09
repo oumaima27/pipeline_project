705074299e4567347931916c6a7be59ddba6aec4
oumaima27 <maghraouioumaima0@gmail.com>
2025-07-09 11:49:39 +0100

    trying the liquibase versioning

--- springboot-swagger-crud/src/main/resources/db/changelog/Scripts/create_tables.sql
+++ springboot-swagger-crud/src/main/resources/db/changelog/Scripts/create_tables.sql
@@ -1,3 +1,3 @@
 INSERT INTO locations (person_id, email, address)
 VALUES
-    ('P0027', 'oumaima@example.com', '123 Maple St, Springfield');
+    ('P0028', 'he@example.com', '123 Maple St, Springfield');
